ab080bf4ea2ae7ddb3ae78a7fbd334601e7528dd
David King <davidkingmwai@gmail.com>
2023-04-04 13:16:37 +0300

    Can fetch an individual tournament

--- src/routes/Tournament.jsx
+++ src/routes/Tournament.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
+import { useParams } from 'react-router-dom'
 
 import './Tournament.css'
 
 const Tournament = () => {
+
+  const { tournamentId } = useParams()
+
+  const [tournament, setTournament] = useState("")
+
+  useEffect(() => {
+    console.log(tournamentId)
+    fetch("http://localhost:8080/api/v1/tournaments/" + tournamentId)
+    .then(res => res.json())
+    .then((data) => {
+      setTournament(data)
+    })
+  }, [])
+
   return (
     <div className='tournament-page'>
-      <h1>Nice</h1>
+      <h1>{tournament.name}</h1>
     </div>
   )
 }
