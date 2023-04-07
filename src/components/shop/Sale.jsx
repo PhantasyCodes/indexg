ff86e22b1e1f7e5e52226b9c0873fd249955e14a
David King <davidkingmwai@gmail.com>
2023-04-07 09:08:00 +0300

    New morning

--- src/components/shop/Sale.jsx
+++ src/components/shop/Sale.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 import salePhoto from '../../assets/images/sale.jpg'
 
@@ -10,9 +11,9 @@ const Sale = () => {
         <div className="sale-text">
             <h1>easter sale</h1>
             <p>Shop selected styles, merch and tickets and get 20% off. Ends April 19th</p>
-            <div className="shop-link2">
+            <motion.div whileHover={{backgroundColor: '#EA1626'}}className="shop-link2">
                 <h2>SHOP</h2>
-            </div>
+            </motion.div>
         </div>
         <img src={salePhoto} alt="sale" />
     </div>
