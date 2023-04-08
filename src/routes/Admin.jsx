a2ad248a7656e25b3dfa3bf9daf833d74b2660a2
David King <davidkingmwai@gmail.com>
2023-04-08 10:37:45 +0300

    Good Morning

--- src/routes/Admin.jsx
+++ src/routes/Admin.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const Admin = () => {
   return (
     <div className='panel'>
-        /
-
+        <div className="panel-section">
+            <h1>Tournaments</h1>
+        </div>
+        <div className="panel-section">
+            <h1>Users</h1>
+        </div>
     </div>
   )
 }
